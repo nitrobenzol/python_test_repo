ac2ca4255385c132044f1c6b4fbf3a9ad495c5f1
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-05-27 05:32:59 -0400

    созданы пакеты для разных зон ответственности (пока только для групп)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application_group import Application
+from model.group import Group
+from fixture.application_group import Application
 
 
 @pytest.fixture
