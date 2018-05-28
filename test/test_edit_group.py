06b40c05d8427d7479bf6c2c320c6167b1d5b45e
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-05-28 06:50:24 -0400

    добавлен тест по модификации группы

---
+++ test/test_edit_group.py
@@ -0,0 +1,8 @@
+from model.group import Group
+
+
+def test_edit_first_group(app):
+
+    app.session.login(username="admin", password="secret")
+    app.group.edit(Group(name="edited name", header="edited header", footer="edited footer"))
+    app.session.logout()
