0bd366f846f086c04d83f4130267ad588b631f54
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-05-28 09:06:38 -0400

    добавлен тест на редактирование контакта (со страницы контактов, нажимая на карандаш)

---
+++ test/test_edit_contact.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+
+def test_test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact("New_Gleb", "New_Sarkisov", "New_Igorevich", "New_Moscow, Veneskaya St, 23, Apt 119", "New_4957166231",
+                        "New_9866662325", "New_123123123", "New_1414141414", "New_glebsarkisov@gmail.com", "New_asdasdasd@com",
+                        "New_wdwdwdwdwdw@gmail.com"))
+    app.session.logout()
