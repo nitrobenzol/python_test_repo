0ebf957ec916f3bf376b5fc16306346736bb65bc
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-03 04:38:44 -0400

    проведена оптимизация ввода данных для теста на редактирование группы, а также выделены вспомогательные методы, благодаря чему уменьшено повторное использование кода

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,15 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
+def test_edit_first_group_name(app):
 
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="edited name", header="edited header", footer="edited footer"))
+    app.group.modify(Group(name="New edited name"))
+    app.session.logout()
+
+
+def test_edit_first_group_header(app):
+
+    app.session.login(username="admin", password="secret")
+    app.group.modify(Group(header="New edited header"))
     app.session.logout()
