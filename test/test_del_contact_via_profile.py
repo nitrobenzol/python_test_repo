db22d04ae52d39d473459559fb534a675463955c
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-05 14:32:52 -0400

    повышена интеллектуальность тестов на редактирование и удаление контактов со страницы профиля контакта посредством добавления проверки на наличие контакта на странице home

--- test/test_del_contact_via_profile.py
+++ test/test_del_contact_via_profile.py
@@ -1,2 +1,7 @@
+from model.contact import Contact
+
+
 def test_del_first_contact_via_profile(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Someone"))
     app.contact.delete_first_contact_via_profile()
--- test/test_edit_contact_via_profile.py
+++ test/test_edit_contact_via_profile.py
@@ -2,6 +2,8 @@ from model.contact import Contact
 
 
 def test_edit_first_contact_via_profile(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Someone"))
     app.contact.edit_first_contact_via_profile(Contact("Edited_Gleb", "Edited_Sarkisov", "Edited_Igorevich", "Edited_Moscow, Veneskaya St, 23, Apt 119", "Edited_4957166231",
                         "Edited_9866662325", "Edited_123123123", "Edited_1414141414", "Edited_glebsarkisov@gmail.com", "Edited_asdasdasd@com",
                         "Edited_wdwdwdwdwdw@gmail.com"))
