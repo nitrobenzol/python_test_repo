21847824a3dcfb7d9dc639e3742423bee921a4ed
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-05 14:06:27 -0400

    повышена интеллектуальность теста на редактирование групп посредством добавления проверки на наличие группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,9 +2,12 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
-
+    if app.group.count() == 0:
+        app.group.create(Group(name="Some name"))
     app.group.modify(Group(name="New edited name"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(header="Some header"))
     app.group.modify(Group(header="New edited header"))
