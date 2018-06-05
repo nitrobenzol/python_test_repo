76611288fdc3ddc4e362d3da054a4db75af088c7
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-05 13:54:10 -0400

    повышена интеллектуальность теста на удаление группы посредством добавления проверки на наличие группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,3 +1,6 @@
-def test_delete_first_group(app):
+from model.group import Group
 
+def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "test"))
     app.group.delete_first_group()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,6 +5,6 @@ def test_edit_first_group_name(app):
 
     app.group.modify(Group(name="New edited name"))
 
-def test_edit_first_group_header(app):
 
+def test_edit_first_group_header(app):
     app.group.modify(Group(header="New edited header"))
