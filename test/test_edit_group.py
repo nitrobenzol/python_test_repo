31556bab531e18bdc927d5c335f2c75f4700dfd7
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-10 15:21:52 -0400

    добавлены проверки на добавление, удаление и изменение групп методом сравнение длины списка групп до действия и после

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,10 +4,17 @@ from model.group import Group
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Some name"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(name="New edited name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="Some header"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(header="New edited header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
