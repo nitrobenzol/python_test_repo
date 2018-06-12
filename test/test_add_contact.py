43bb58bfc3d157ba26f6d80acf70cbed3cdb99bd
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-12 11:44:48 -0400

    реализована проверка для теста на редактирование контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,12 +5,8 @@ def test_add_contact(app):
     contact = Contact("Gleb", "Sarkisov", "Igorevich", "Moscow, Veneskaya St, 23, Apt 119", "4957166231",
                         "9866662325", "123123123", "1414141414", "glebsarkisov@gmail.com", "asdasdasd@com",
                         "wdwdwdwdwdw@gmail.com")
-    # get list of all contacts presented before create action - old_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
-    # get list of all contacts presented after create action - new_contacts
     new_contacts = app.contact.get_contacts_list()
-    # add new contact to old list of contacts
     old_contacts.append(contact)
-    # check if old_contacts + contact == new_contacts
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,11 +2,17 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    contact = Contact("New Gleb", "New Sarkisov", "New Igorevich", "New Moscow, Veneskaya St, 23, Apt 119", "New 4957166231",
+                        "New 9866662325", "New 123123123", "New 1414141414", "New glebsarkisov@gmail.com", "New asdasdasd@com",
+                        "New wdwdwdwdwdw@gmail.com")
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="Jack"))
-    app.contact.edit_first_contact(Contact("New Gleb", "New Sarkisov", "New Igorevich", "New Moscow, Veneskaya St, 23, Apt 119", "New 4957166231",
-                        "New 9866662325", "New 123123123", "New 1414141414", "New glebsarkisov@gmail.com", "New asdasdasd@com",
-                        "New wdwdwdwdwdw@gmail.com"))
+    old_contacts = app.contact.get_contacts_list()
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contacts_list()
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_edit_first_name(app):
