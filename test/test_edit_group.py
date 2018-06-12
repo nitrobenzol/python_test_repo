d9d51229bab0521d20907024e8af61f4ae83d00a
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-12 06:16:46 -0400

    реализованы проверки для всех тестов на добавление, редактирование и удаление групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,19 +2,22 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    group = Group(name="Some name")
     if app.group.count() == 0:
-        app.group.create(Group(name="Some name"))
+        app.group.create(group)
     old_groups = app.group.get_group_list()
-    app.group.modify(Group(name="New edited name"))
+    group.id = old_groups[0].id
+    app.group.modify(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert old_groups == new_groups
 
 
-
-def test_edit_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header="Some header"))
-    old_groups = app.group.get_group_list()
-    app.group.modify(Group(header="New edited header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_edit_first_group_header(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(header="Some header"))
+#    old_groups = app.group.get_group_list()
+#    app.group.modify(Group(header="New edited header"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
