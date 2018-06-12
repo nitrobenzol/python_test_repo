224157fdda13160f2d87633edfae1ded87ad632c
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-12 11:38:18 -0400

    реализована проверка для теста на удаление контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,6 +2,11 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    old_contacts = app.contact.get_contacts_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="Glebus"))
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contacts_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
