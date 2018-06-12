f2ce85fc76087a5a2027e082e43bc5dc8f02b916
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-12 11:55:46 -0400

    реализованы недостающие проверки для всех тестов на добавление, модификацию и удаление контактов

--- test/test_del_contact_via_profile.py
+++ test/test_del_contact_via_profile.py
@@ -4,4 +4,9 @@ from model.contact import Contact
 def test_del_first_contact_via_profile(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="Someone"))
+    old_contacts = app.contact.get_contacts_list()
     app.contact.delete_first_contact_via_profile()
+    new_contacts = app.contact.get_contacts_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
--- test/test_edit_contact_via_profile.py
+++ test/test_edit_contact_via_profile.py
@@ -2,8 +2,14 @@ from model.contact import Contact
 
 
 def test_edit_first_contact_via_profile(app):
+    contact = Contact("Edited_Gleb", "Edited_Sarkisov", "Edited_Igorevich", "Edited_Moscow, Veneskaya St, 23, Apt 119", "Edited_4957166231",
+                        "Edited_9866662325", "Edited_123123123", "Edited_1414141414", "Edited_glebsarkisov@gmail.com", "Edited_asdasdasd@com",
+                        "Edited_wdwdwdwdwdw@gmail.com")
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="Someone"))
-    app.contact.edit_first_contact_via_profile(Contact("Edited_Gleb", "Edited_Sarkisov", "Edited_Igorevich", "Edited_Moscow, Veneskaya St, 23, Apt 119", "Edited_4957166231",
-                        "Edited_9866662325", "Edited_123123123", "Edited_1414141414", "Edited_glebsarkisov@gmail.com", "Edited_asdasdasd@com",
-                        "Edited_wdwdwdwdwdw@gmail.com"))
+    old_contacts = app.contact.get_contacts_list()
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact_via_profile(contact)
+    new_contacts = app.contact.get_contacts_list()
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
