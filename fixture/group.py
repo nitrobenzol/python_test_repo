2f027f2a2d8c861a4bbf2ab2ca1f1159ea1bf105
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-14 16:36:01 -0400

    добавлена случайность выбора модифицируемой группы

--- fixture/group.py
+++ fixture/group.py
@@ -55,10 +55,13 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
-    def modify(self, new_group_data):
+    def modify(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # init editing
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -67,7 +70,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
-
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,18 @@
 from model.group import Group
+from random import randrange
 
 
-def test_edit_first_group_name(app):
+def test_edit_some_group_name(app):
     group = Group(name="Some name")
     if app.group.count() == 0:
         app.group.create(group)
     old_groups = app.group.get_group_list()
-    group.id = old_groups[0].id
-    app.group.modify(group)
+    index = randrange(len(old_groups))
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
