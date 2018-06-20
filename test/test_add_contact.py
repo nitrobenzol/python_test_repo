4d33f757b7131745de3c142c3131005711c1343b
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-06-20 03:52:25 -0400

    параметризован тест на добавление контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,27 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
-    contact = Contact(first_name="Gleb", last_name="Sarkisov", middle_name="Igorevich", address="Moscow, Veneskaya St, 23, Apt 119", home_phone="4957166231",
-                        mobile_phone="9866662325", work_phone="123123123", secondary_phone="1414141414", email="glebsarkisov@gmail.com", email2="asdasdasd@com",
-                        email3="wdwdwdwdwdw@gmail.com")
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(first_name="", last_name="", middle_name="", address="", home_phone="",
+                    mobile_phone="", work_phone="", secondary_phone="", email="", email2="",
+                    email3="")] + [
+    Contact(first_name=random_string("first_name", 10), last_name=random_string("last_name", 10),
+            middle_name=random_string("middle_name", 10), address=random_string("address", 20),
+            home_phone=random_string("home_phone", 15), mobile_phone=random_string("mobile_phone", 10),
+            work_phone=random_string("work_phone", 20), secondary_phone=random_string("sec_phone", 20),
+            email=random_string("email1", 10), email2=random_string("email2", 10), email3=random_string("email3", 10))
+    for i in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
