c9eb7fb322d45128c9a3238cc4edca654204bf77
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-07-01 15:52:13 -0400

    реализованы функции для получения контактов в выбранной группе и списка всех контактов, не состоящих в данной группе

--- check_db_connection.py
+++ check_db_connection.py
@@ -4,7 +4,7 @@ from model.group import Group
 db = ORMFixture(host="localhost", unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contacts_in_group(Group(id='289'))
+    l = db.get_contacts_not_in_group(Group(id='289'))
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -45,13 +45,19 @@ class ORMFixture:
 
     @db_session
     def get_group_list(self):
-        return self.convert_groups_to_model(list(select(g for g in ORMFixture.ORMGroup)))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
     @db_session
     def get_contact_list(self):
-        return self.convert_contacts_to_model(list(select(c for c in ORMFixture.ORMContact if c.deprecated is None)))
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
 
     @db_session
     def get_contacts_in_group(self, group):
         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
