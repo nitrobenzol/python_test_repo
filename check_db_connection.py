0df8c42b2a0738141d5f82a22e7a410898cf6ed1
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-07-01 11:04:11 -0400

    реализована связка структуры базы данных с соответствующими объектами приложения - контактами и группами

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,11 @@
-import pymysql
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-db = DbFixture(host="localhost", unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", name="addressbook", user="root", password="")
+db = ORMFixture(host="localhost", unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass
