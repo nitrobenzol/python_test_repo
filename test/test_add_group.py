11d5429af310db286412c539a7bb095b8c12d206
Gleb Sarkisov <glebsarkisov@gmail.com>
2018-07-23 16:45:53 -0400

    исправлена ошибка

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,7 +8,7 @@ def test_add_group(app, db, json_groups, check_ui):
         old_groups = db.get_group_list()
     with pytest.allure.step('When I add a group %s to the list' % group):
         app.group.create(group)
-    with pytest.allure.step('Then the new group list is equal to the old list with the added group')
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
         new_groups = db.get_group_list()
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
